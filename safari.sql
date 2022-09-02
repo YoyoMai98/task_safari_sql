08ef8aa0f7e9a63e38046f16d4013c2e1f9337a1
Hanqing Huang <h.hanqing@yahoo.com>
2022-09-02 14:59:33 +0100

    populate tables

--- safari.sql
+++ safari.sql
@@ -1,4 +1,7 @@
-
+DROP TABLE assignments;
+DROP TABLE animals;
+DROP TABLE enclosures;
+DROP TABLE staff;
 
 CREATE TABLE staff(
     id SERIAL PRIMARY KEY,
@@ -27,3 +30,22 @@ CREATE TABLE assignments(
     enclosureId INT REFERENCES enclosures(id),
     day VARCHAR(255)
 );
+-- populate with data
+--STAFF
+INSERT INTO staff (name, employeeNumber) VALUES ('Yongran', 1);
+INSERT INTO staff (name, employeeNumber) VALUES ('Hanqing', 2);
+--ENCLOSURES
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('Parrots', 5, false);
+INSERT INTO enclosures (name, capacity, closedForMaintenance) VALUES ('Giraffes', 2, false);
+--ANIMALS
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Melvin', 'Giraffe', 4, 2);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Rachel', 'Giraffe', 1, 2);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Polly', 'Parrot', 1, 1);
+--ASSIGNMENTS
+INSERT INTO assignments (employeeId, enclosureId, day) VALUES (1, 1, 'Friday');
+INSERT INTO assignments (employeeId, enclosureId, day) VALUES (2, 2, 'Friday');
+
+SELECT * FROM staff;
+SELECT * FROM enclosures;
+SELECT * FROM animals;
+SELECT * FROM assignments;
